99a2ff5b87990c0f4e5624ba7e5cc3cc1bbd79d7
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-10 11:24:06 +0300

    Static Properties & Methods

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,17 +17,19 @@ class ShoppingCart {
 
 	addProduct(product) {
 		this.items.push(product);
-		this.totalOutput = `<h2>Total: \$${1}</h2>`;
+		this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
 	}
 
 	render() {
 		const cartEl = document.createElement('section');
 		cartEl.className = 'cart';
+
 		cartEl.innerHTML = `	
 			<h2>Total: \$${0}</h2>
 			<button>Order Now!</button>
 		`;
-		const totalOutput = cartEl.querySelector('h2');
+
+		this.totalOutput = cartEl.querySelector('h2');
 		return cartEl;
 	}
 }
@@ -38,8 +40,7 @@ class ProductItem {
 	}
 
 	addToCart() {
-		console.log('Added to Cart...');
-		console.log(this.product);
+		App.addProductToCart(this.product);
 	}
 
 	render() {
@@ -66,13 +67,13 @@ class ProductList {
 	products = [
 		new Product(
 			'A Robot',
-			'https://images.unsplash.com/photo-1563207153-f403bf289096?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+			'https://images.unsplash.com/photo-1494390248081-4e521a5940db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
 			'A swift and ingenious machine that has the ability to learn.',
 			44.99
 		),
 		new Product(
 			'A Bunch of Cherries',
-			'https://images.unsplash.com/photo-1580926608062-464f40bdcec2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+			'https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
 			'Delicious and nutritive fruit plate.',
 			77.99
 		),
@@ -96,8 +97,8 @@ class ProductList {
 class Shop {
 	render() {
 		const renderHook = document.getElementById('app');
-		const cart = new ShoppingCart();
-		const cartEl = cart.render();
+		this.cart = new ShoppingCart();
+		const cartEl = this.cart.render();
 		const productList = new ProductList();
 		const prodListEl = productList.render();
 
@@ -106,5 +107,18 @@ class Shop {
 	}
 }
 
-const shop = new Shop();
-shop.render();
+class App {
+	static cart;
+
+	static init() {
+		const shop = new Shop();
+		shop.render();
+		this.cart = shop.cart;
+	}
+
+	static addProductToCart(product) {
+		this.cart.addProduct(product);
+	}
+}
+
+App.init();
