28fe39b2c8a1250f4e6deeb34741ba97d2bc03ad
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-10 10:18:20 +0300

    Using & Connecting Multiple Classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 class Product {
-	title = 'DEFAULT';
-	imageURL;
-	description;
-	price;
+	// title = 'DEFAULT';
+	// imageURL;
+	// description;
+	// price;
 
 	constructor(title, image, desc, price) {
 		this.title = title;
@@ -12,8 +12,31 @@ class Product {
 	}
 }
 
-const productList = {
-	products: [
+class ProductItem {
+	constructor(product) {
+		this.product = product;
+	}
+
+	render() {
+		const prodEl = document.createElement('li');
+		prodEl.className = 'product-item';
+		prodEl.innerHTML = `
+					<div>
+						<img src="${this.product.imageURL}" alt="${this.product.title}" />
+						<div class="product-item__content">
+							<h2>${this.product.title}</h2>
+							<h3>\$${this.product.price}</h3>
+							<p>${this.product.description}</p>
+							<button>Add To Cart</button>
+						</div>
+					</div>
+			`;
+		return prodEl;
+	}
+}
+
+class ProductList {
+	products = [
 		new Product(
 			'A Robot',
 			'https://images.unsplash.com/photo-1563207153-f403bf289096?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
@@ -26,30 +49,23 @@ const productList = {
 			'Delicious and nutritive fruit plate.',
 			77.99
 		),
-	],
+	];
+
+	constructor() {}
+
 	render() {
 		const renderHook = document.getElementById('app');
 		const prodList = document.createElement('ul');
 		prodList.className = 'product-list';
 
 		for (const prod of this.products) {
-			const prodEl = document.createElement('li');
-			prodEl.className = 'product-item';
-			prodEl.innerHTML = `
-                <div>
-                    <img src="${prod.imageURL}" alt="${prod.title}" />
-                    <div class="product-item__content">
-                        <h2>${prod.title}</h2>
-                        <h3>\$${prod.price}</h3>
-                        <p>${prod.description}</p>
-                        <button>Add To Cart</button>
-                    </div>
-                </div>
-            `;
+			const productItem = new ProductItem(prod);
+			const prodEl = productItem.render();
 			prodList.append(prodEl);
 		}
 		renderHook.append(prodList);
-	},
-};
+	}
+}
 
+const productList = new ProductList();
 productList.render();
