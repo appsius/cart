9a72869b985cba28a50a3dceaf130b92cb9049ef
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-10 18:38:34 +0300

    Different Ways of Adding Methods

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -58,7 +58,12 @@ class ShoppingCart extends Component {
 	}
 
 	constructor(renderHookId) {
-		super(renderHookId);
+		super(renderHookId, false);
+		this.orderProducts = () => {
+			console.log('Order...');
+			console.log(this.items);
+		};
+		this.render();
 	}
 
 	addProduct(product) {
@@ -73,6 +78,9 @@ class ShoppingCart extends Component {
 			<h2>Total: \$${0}</h2>
 			<button>Order Now!</button>
 		`;
+		const orderButton = cartEl.querySelector('button');
+		orderButton.addEventListener('click', () => this.orderProducts());
+		orderButton.addEventListener('click', this.orderProducts);
 		this.totalOutput = cartEl.querySelector('h2');
 	}
 }
